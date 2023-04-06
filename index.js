36060c319160d69973137c17ad8f2de6b2afc85b
patriciadania <patricia.adania@hotmail.com>
2023-04-06 15:11:12 -0300

    Implementação da primeira função

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
-};
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8';
+    fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(trataErro)
+}
+pegaArquivo('./arquivos/texto.md');
+
+//\(https?:\/\/[^\s?#.].[^\s]*\)
