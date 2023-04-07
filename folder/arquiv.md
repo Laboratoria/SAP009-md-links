3e3ddc3424125c9b9b7447f44aaab068a3300700
Ana <ana_leticia@geb.inatel.br>
2023-04-07 10:12:17 -0300

    criação da função md-links

---
+++ folder/arquiv.md
@@ -0,0 +1,6 @@
+## 1. Oi, deu certo!
+
+Está aparecendo!
+[links1](https:www.teste1.com.br)
+[links2](https:www.teste2.com)
+[links3](https:www.teste3.com)
--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-function init(){
-  console.log('deu certo, olha eu aqui');
+// function init(){
+//   console.log('deu certo, olha eu aqui');
+// }
+// module.exports = init;
+const fs = require('fs');
+
+function lerArquivo(pathFile){
+  fs.readFile(pathFile, 'utf8', (err, data) => {
+    // console.log(data);
+    if (err) throw err;
+
+    const defaultRegex = /\[\w+.\w+\]\(\w+.+\)/gmi;
+    const searchLinks = data.match(defaultRegex);
+    console.log(searchLinks);
+    searchLinks.forEach(link => {
+      console.log(link);
+    });
+  });
 }
-module.exports = init;
+
+lerArquivo('./folder/arquiv.md');
