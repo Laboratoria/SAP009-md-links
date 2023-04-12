0922ad7e9a9eeb89720c9562c0829e4d7ee860f1
Ana <ana_leticia@geb.inatel.br>
2023-04-12 18:27:39 -0300

    estruturacao da função fetch

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-// function init(){
-//   console.log('deu certo, olha eu aqui');
-// }
-// module.exports = init;
 const fs = require('fs');
+const chalk = require('chalk');
+const fetch = require('node-fetch');
 
-function lerArquivo(pathFile){
+function mdlinks(pathFile){
+
+  const fileExists = fs.existsSync(pathFile); //funções do fs p/ verificar infos sobre arquivos.
+  const fileSize = fs.statSync(pathFile).size;
+  if(fileExists === false || fileSize === 0){
+    console.log(chalk.red('\u2717') + ' ' + `O arquivo ${chalk.red(pathFile)} está vazio ou não existe.`);
+    return; // encerra a execução após imprimir, senão, da erro no foreach
+  }
   fs.readFile(pathFile, 'utf8', (err, data) => {
-    // console.log(data);
+    
     if (err) throw err;
-
     const defaultRegex = /\[\w+.\w+\]\(\w+.+\)/gmi; 
     const searchLinks = data.match(defaultRegex);
-
     searchLinks.forEach(link => {
       const removeItens = link.replace(')','').replace('[','');
       const split = removeItens.split('](');
@@ -20,9 +23,24 @@ function lerArquivo(pathFile){
         text: split[0],
         file: pathFile,
       }
-      console.log(newObj.file + ' ' + newObj.href + ' ' + newObj.text);
+      httpFetch(newObj.href);
+      console.log(chalk.red('\u2764') + ' ' + chalk.magenta(newObj.file) + ' ' + chalk.gray(newObj.href) + ' ' + chalk.magenta(newObj.text));
     });
   });
 }
 
-lerArquivo('README.md');
+module.exports = mdlinks;
+
+function httpFetch(urlPath){
+  if (urlPath === false) {
+    return;
+  } 
+  fetch(urlPath)
+    .then(answer => {
+      // console.log(urlPath);
+      console.log(answer.status);
+      console.log(answer.ok);
+    })
+    .catch(err => console.error(err));
+}
+
