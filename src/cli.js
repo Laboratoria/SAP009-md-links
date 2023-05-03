f6d610e801f56893d0830ba021749017109b4849
osikmelina <melina.mk@gmail.com>
2023-05-03 12:10:35 -0300

    lista de links com validate ok e função stats em andamento

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
 // cli -> manipulação das infos vindas da linha de comando do terminal
 // de acordo com as funções criadas no index para entregar o resultado
 import chalk from 'chalk';
 import fs from 'fs';
-import pegaArquivo from './index.js';
+import extraiLinks from './index.js';
 import listaValidada from './http-validacao.js';
 
 // varivável que guarda as chegadas do terminal
@@ -10,19 +11,25 @@ import listaValidada from './http-validacao.js';
 const caminho = process.argv;
 
 // função p/ refatorar os console.logs com retorno desse resultado
-async function imprimeLista(valida, resultado, identificador = '') {
+async function imprimeLista(valida, resultado = '') {
   if (valida) {
-    console.log(
-      chalk.yellow('lista validada'), 
-      // identificador vai receber o nome do arquivo pra imprirmir junto com o link
-      chalk.black.bgGreen(identificador),
-      await listaValidada(resultado));
+    const linha = await listaValidada(resultado);
+    linha.forEach((link) => {
+      console.log(
+        `${link.file} | ${link.href} | ${link.text} | ${link.status}`,
+      );
+    });
   } else {
-  console.log(
-    chalk.yellow('lista validada'), 
-    // identificador vai receber o nome do arquivo pra imprirmir junto com o link
-    chalk.black.bgGreen(identificador),
-    resultado);
+    resultado.forEach((link) => {
+      console.log(
+        `${link.file} | ${link.href} | ${link.text}`,
+      );
+    });
+  }
+  if (process.argv.includes('--stats')) {
+    const linksUnicos = new Set(resultado.map((link) => link.href));
+    console.log(chalk.bgBlueBright(`total de links: ${resultado.length}`));
+    console.log(chalk.bgBlue(`links únicos: ${linksUnicos.size}`));
   }
 }
 
@@ -31,13 +38,13 @@ async function imprimeLista(valida, resultado, identificador = '') {
 function processaTexto(argumentos) {
   // pegando o index 2 pois os dois primeiros itens são somente
   // o local onde ele encontrou infos e o terceiro é o objeto em si
-  const caminho = argumentos[2];
-  //guardando o 4o elemento do array, que será o comando p/ executar o que o usuário quer
-  const valida = argumentos[3] === '--valida';
- 
+  const caminhoArgumento = argumentos[2];
+  // guardando o 4o elemento do array, que será o comando p/ executar o que o usuário quer
+  const valida = argumentos[3] === '--validate';
+
   // testa se o caminho está correto
   try {
-    fs.lstatSync(caminho);
+    fs.lstatSync(caminhoArgumento);
   // se cair no erro, aparece o erro ENOENT no terminal, então o if é p/
   //  alterar ele p/ um aviso compreensível p/ o usuário
   } catch (erro) {
@@ -48,32 +55,33 @@ function processaTexto(argumentos) {
   }
 
   // isFile retorna true se for o caminho de arquivo e false se não for
-  if (fs.lstatSync(caminho).isFile()) {
-    pegaArquivo(argumentos[2])
+  if (fs.lstatSync(caminhoArgumento).isFile()) {
+    extraiLinks(argumentos[2])
     // resultado chamará a pegaArquivo passando <caminho> na posição 2 do array
       .then((resultado) => {
-      imprimeLista(valida, resultado)
-    })
+        imprimeLista(valida, resultado);
+      })
       .catch((erro) => {
-      console.error('Erro ao processar o arquivo', erro);
-    });
+        console.error('Erro ao processar o arquivo', erro);
+      });
     // caso não seja um caminho de arquivo, verifica se é um caminho de um diretório
   } else if (fs.lstatSync(caminho).isDirectory()) {
     fs.promises.readdir(caminho)
     // caso seja um diretório, ele retorna os arquivos dentro desse diretório
       .then((arquivos) => {
         arquivos.forEach((nomeDeArquivo) => {
-          pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+          extraiLinks(`${caminho}/${nomeDeArquivo}`)
             .then((lista) => {
-            imprimeLista(valida, lista, nomeDeArquivo)
-          })
-        })
+              imprimeLista(valida, lista, nomeDeArquivo);
+            });
+        });
       })
       .catch((err) => {
         console.error(err);
       });
   }
 }
-  processaTexto(caminho)
+processaTexto(caminho);
 
-  // o then usa uma função de callback dentro originária da função na qual ele tá inserido p então executar sua ação
+// o then usa uma função de callback dentro originária da função na qual
+// ele tá inserido p então executar sua ação
--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,50 +1,48 @@
 import chalk from 'chalk';
 
-function extraiLinks (arrayLinks) {
+function extraiLinks(arrayLinks) {
   // Object.values retorna somente o valor do intem dentro do objeto
   // e o item desse objeto com valor é o link, pois está numa string dentro desse objeto
-  return arrayLinks.map((objetoLink) => Object.values(objetoLink).join())
+  return arrayLinks.map((objetoLink) => objetoLink.href);
 }
 
-// função p/ checar o status 
+function manejaErros(erro) {
+  if (erro.cause.code === 'ENOTFOUND') {
+    return (chalk.magenta('link não encontrado'));
+  }
+  return 'ocorreu algum erro';
+}
+
+// função p/ checar o status
 // uma lista será sempre um array iterável
-function checkStatus (listaURLs) {
-  // fetch lida c/ uma coisa de cada vez, então precisou primeiro 
+function checkStatus(listaURLs) {
+  // fetch lida c/ uma coisa de cada vez, então precisou primeiro
   // await todas as promises p/ aí sim retornar a lista de URLs
-  const arrayStatus = Promise
-  .all(
+  const arrayStatus = Promise.all(
     // map está encontrando os links e depois usando o fetch p/ retornar
     // o código de status (200, 404, etc)
-    listaURLs.map(async (url) => {
+    listaURLs.map((url) => fetch(url)
       // o retorno do fetch é sempre uma promessa, por isso sempre assíncrona
-          return fetch(url)
-          .then ((response) => {
-            return `${response.status} - ${response.statusText}`;
-          })
-          // mesmo o link com erro vai para a lista impressa pois está dentro do map
-          .catch ((erro) => {
-            return manejaErros(erro);
-          })
-        })
-      )
+      .then((response) => {
+        if (response.status === 200) {
+          return (chalk.bgBlue(` ${response.status} | ${'OK'} `));
+        }
+        if (response.status === 404 || 'Not Found') {
+          return (chalk.bgMagenta(` ${response.status} | ${'FAIL'} `));
+        }
+        return `${response.status} - ${response.statusText}`;
+      })
+      // mesmo o link com erro vai para a lista impressa pois está dentro do map
+      .catch((erro) => manejaErros(erro))),
+  );
   return arrayStatus;
 }
 
-function manejaErros (erro) {
-  if (erro.cause.code === 'ENOTFOUND') {
-    return 'link não encontrado';
-  } else {
-    return 'ocorreu algum erro';
-  }
-}
-
-export default async function listaValidada (listaDeLinks) {
+export default async function listaValidada(listaDeLinks) {
   const links = extraiLinks(listaDeLinks);
   return checkStatus(links)
-  .then ((status) => {
-    return listaDeLinks.map((objeto, indice) => ({
+    .then((status) => listaDeLinks.map((objeto, indice) => ({
       ...objeto,
-      status: status[indice]
-    }))
-  })
+      status: status[indice],
+    })));
 }
