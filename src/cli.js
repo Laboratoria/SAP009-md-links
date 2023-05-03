22bd3e488df99f86a6a9d81b679c25ab9c5a6e6f
Polyana <polyanacristina2001@gmail.com>
2023-05-03 18:48:37 -0300

    importação do fetch e construção das flags --validate e --stats

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@
 import { mdLinks } from './md-links.js';
 import chalk from 'chalk';
 import process from 'process';
+import fetch from 'node-fetch';
 
 const caminhoDoArquivo = process.argv[2];
 
@@ -15,23 +16,36 @@ if (options.validate) {
   mdLinks(caminhoDoArquivo, { validate: true })
     .then((informacoes) => {
       informacoes.map((item) => {
-        const status = item.status === 200 ? chalk.green('ok') : chalk.red('fail');
-        console.log(`${chalk.blue(item.arquivo)} ${chalk.cyan(item.link)} ${chalk.green(item.texto)} ${status}`);
+        fetch(item.link)
+          .then((res) => {
+            const status = res.status === 200 ? chalk.green('ok') : chalk.red('fail');
+            console.log(`${chalk.blue(item.arquivo)} ${chalk.cyan(item.link)} ${chalk.yellow(status)} ${chalk.yellow(res.status)} ${chalk.green(item.texto)} `);
+          })
+          .catch((err) => {
+            console.log(`${chalk.blue(item.arquivo)} ${chalk.cyan(item.link)} ${chalk.red('fail')} ${chalk.red(err)} ${chalk.green(item.texto)} `);
+          });
       });
     })
     .catch((err) => {
       console.log(err);
     });
-} else {
-  console.log('teste')
+} else if (options.stats) {
+  mdLinks(caminhoDoArquivo, { stats: true })
+  .then((informacoes) => {
+    const links = informacoes.map((item) => item.link);
+    console.log(`Total: ${informacoes.length}`);
+    console.log(`Unique: ${links.length}`);
+  }).catch((err) => {
+    console.log(err);
+  });
+} else if (options.validate && options.stats) {
+  mdLinks(caminhoDoArquivo, { validate: true, stats: true})
+  .then((informacoes) => {
+    const links = informacoes.map((item) => item.link);
+    console.log(`Total: ${informacoes.length}`);
+    console.log(`Unique: ${links.length}`);
+  }).catch((err) => {
+    console.log(err);
+  });
 }
 
-
-// mdLinks(caminhoDoArquivo, options)
-//   .then((informacoes) => {
-//     informacoes.map((item) => {
-//     console.log(`${chalk.blue(item.arquivo)} ${chalk.cyan(item.link)} ${chalk.green(item.texto)}`)
-//     })
-//   }).catch((err) => {
-//       console.log(err);
-//   })
--- src/texto.md
+++ src/texto.md
@@ -4,8 +4,7 @@ manipulam texto (GitHub, fórum, blogs e etc) e é muito comum encontrar arquivo
 com este formato em qualquer repositório (começando pelo tradicional
 `README.md`).
 Uma comunidade open source nos propôs criar uma ferramenta, usando
-[Node.js](https://nodejs.org/), que leia e analise arquivos no formato
+[Node.js](https://api.github.com/users/manishmshiv), que leia e analise arquivos no formato
 `Markdown`, para verificar os arquivos que contenham links e mostrar algumas
 estatísticas.
 
-![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
