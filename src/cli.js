461bd8c072eb99afa9a5e66755feb0cf116e6403
Polyana <polyanacristina2001@gmail.com>
2023-05-04 15:05:05 -0300

    finalização das flags --validate e --stats

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import { mdLinks } from './md-links.js';
 import chalk from 'chalk';
 import process from 'process';
@@ -12,7 +11,37 @@ const options = {
   stats: process.argv.includes('--stats')
 };
 
-if (options.validate) {
+if (options.validate && options.stats) {
+  mdLinks(caminhoDoArquivo, { validate: true })
+    .then((informacoes) => {
+      const links = informacoes.map((item) => item.link);
+      const linksQuebrados = [];
+      //Promise.all() é um método que recebe um array de promessas e retorna uma nova promessa que é resolvida somente quando todas as promessas do array forem resolvidas.
+      Promise.all(informacoes.map((item) =>
+        fetch(item.link)
+          .then((res) => {
+            if (res.status !== 200) {
+              linksQuebrados.push(item.link);
+            }
+          })
+          .catch(() => {
+            linksQuebrados.push(item.link);
+          })
+      ))
+        .then(() => {
+          console.log(`Total: ${informacoes.length}`);
+          console.log(`Unique: ${links.length}`);
+          console.log(`Broken: ${linksQuebrados.length}`);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+} else if (options.validate) {
   mdLinks(caminhoDoArquivo, { validate: true })
     .then((informacoes) => {
       informacoes.map((item) => {
@@ -29,23 +58,13 @@ if (options.validate) {
     .catch((err) => {
       console.log(err);
     });
+
 } else if (options.stats) {
   mdLinks(caminhoDoArquivo, { stats: true })
-  .then((informacoes) => {
-    const links = informacoes.map((item) => item.link);
-    console.log(`Total: ${informacoes.length}`);
-    console.log(`Unique: ${links.length}`);
-  }).catch((err) => {
-    console.log(err);
-  });
-} else if (options.validate && options.stats) {
-  mdLinks(caminhoDoArquivo, { validate: true, stats: true})
-  .then((informacoes) => {
-    const links = informacoes.map((item) => item.link);
-    console.log(`Total: ${informacoes.length}`);
-    console.log(`Unique: ${links.length}`);
-  }).catch((err) => {
-    console.log(err);
-  });
-}
+    .then((informacoes) => {
+      const links = informacoes.map((item) => item.link);
+      console.log(`Total: ${informacoes.length}`);
+      console.log(`Unique: ${links.length}`);
+    })
+  }
 
--- src/texto.md
+++ src/texto.md
@@ -1,10 +1,15 @@
-[Markdown](https://pt.wikipedia.org/wiki/Markdown) é uma linguagem de marcação
-muito popular entre os programadores. É usada em muitas plataformas que
-manipulam texto (GitHub, fórum, blogs e etc) e é muito comum encontrar arquivos
-com este formato em qualquer repositório (começando pelo tradicional
-`README.md`).
-Uma comunidade open source nos propôs criar uma ferramenta, usando
-[Node.js](https://api.github.com/users/manishmshiv), que leia e analise arquivos no formato
-`Markdown`, para verificar os arquivos que contenham links e mostrar algumas
-estatísticas.
+[Markdown](https://pt.wikipedia.org/wiki/Markdown)
 
+[Node.js](https://api.github.com/users/manishmshiv)
+
+[md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
+
+[Sort](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
+
+[ForEach](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach)
+
+[Map](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map)
+
+[Filter](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/filtrozinho)
+
+[Reduce](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/Reducezinho)
