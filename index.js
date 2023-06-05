a47228140886baa08a276327ca95d486e34f7fb7
Tharla <jaroxeski2021@gmail.com>
2023-06-05 19:21:53 -0300

    foi feito o looping no matches

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+
 function readMarkdownFile(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (error, data) => {
@@ -7,12 +8,71 @@ function readMarkdownFile(filePath) {
         reject(error);
       } else {
         const regex = /\[([^\]]+)\]\(([^)]+)\)/g;
-      const matches = data.match(regex);
-        resolve(matches);
+        const matches = data.match(regex);
+        const result = [];
+
+        if (matches) {
+          for (let i = 0; i < matches.length; i++) {
+            const match = matches[i];
+            const linkText = match.replace(regex, '$1');
+            const linkUrl = match.replace(regex, '$2');
+            const item = {
+              text: linkText,
+              url: linkUrl
+            };
+            result.push(item);
+          }
+        }
+
+        resolve(result);
       }
     });
   });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function readMarkdownFile(filePath) {
+//  return new Promise((resolve, reject) => {
+  //  fs.readFile(filePath, 'utf8', (error, data) => {
+   //   if (error) {
+    //    reject(error);
+    //  } else {
+     //   const regex = /\[([^\]]+)\]\(([^)]+)\)/g;
+     // const matches = data.match(regex);
+     //   resolve(matches);
+     // }
+  //  });
+  //});
+//}
 
 
 
